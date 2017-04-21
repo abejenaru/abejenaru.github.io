c145acfd276cb3ee8f4793d951718e6d192c1373
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2017-04-21 15:51:13 +0200

    Initial commit

---
+++ js/specs.js
@@ -0,0 +1,95 @@
+
+var vgEmbedConfig = { "mode": "vega", "renderer": "canvas", "actions": { "source": false, "editor": false } };
+
+
+var visualizationSpec = {
+  "$schema": "https://vega.github.io/schema/vega/v3.0.json",
+
+  "width": 1600,
+  "height": 600,
+  "padding": 2,
+  "autosize": "fit",
+
+  "legends": [
+    {"title": "", "fill": "color"}
+  ],
+
+  "data": [
+    {
+      "name": "raw-data",
+      "url": "data/activity.csv",
+      "format": {"type":"csv", "parse": {"Date": "date", "LightlyActiveMinutes": "number", "FairlyActiveMinutes": "number", "VeryActiveMinutes": "number"}}
+    },
+    {
+      "name": "table",
+      "source": "raw-data",
+      "transform": [
+        {"type": "fold", "fields": ["LightlyActiveMinutes","FairlyActiveMinutes","VeryActiveMinutes"]}
+      ]
+    }
+  ],
+
+  "scales": [
+    {
+      "name": "x",
+      "type": "time",
+      "range": "width",
+      "round": true,
+      "domain": {"data": "table", "field": "Date"}
+    },
+    {
+      "name": "y",
+      "type": "linear",
+      "range": "height",
+      "nice": true,
+      "zero": true,
+      "domain": {"data": "table", "field": "value"}
+    },
+    {
+      "name": "color",
+      "type": "ordinal",
+      "range": "category",
+      "domain": {"data": "table", "field": "key"}
+    }
+  ],
+
+  "axes": [
+    {"orient": "bottom", "scale": "x", "format": "%b %d, %Y"},
+    {"orient": "left", "scale": "y", "grid": true}
+  ],
+
+  "marks": [
+    {
+      "type": "group",
+      "from": {
+        "facet": {
+          "name": "series",
+          "data": "table",
+          "groupby": "key"
+        }
+      },
+      "marks": [
+        {
+          "type": "line",
+          "from": {"data": "series"},
+          "encode": {
+            "enter": {
+              "interpolate": {"value": "linear"},
+              "x": {"scale": "x", "field": "Date"},
+              "y": {"scale": "y", "field": "value"},
+              "y2": {"scale": "y", "value": 0},
+              "stroke": {"scale": "color", "field": "key"},
+              "strokeWidth": {"value": 2}
+            },
+            "update": {
+              "strokeWidth": {"value": 2}
+            },
+            "hover": {
+              "strokeWidth": {"value": 5}
+            }
+          }
+        }
+      ]
+    }
+  ]
+};
